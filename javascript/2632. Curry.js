aa6a48d84c0c21496bfc7696db2593e19162809a
Daniel Perilla <daniperil@hotmail.com>
2023-05-31 17:54:41 -0500

    Create 2632. Curry.js

---
+++ javascript/2632. Curry.js
@@ -0,0 +1,36 @@
+/**
+ *Problem: 2632. Curry
+ *Given a function fn, return a curried version of that function.
+ *A curried function is a function that accepts fewer or an equal number of parameters as the original function and returns either another curried function or the same value the original function would have returned.
+ *In practical terms, if you called the original function like sum(1,2,3), you would call the curried version like csum(1)(2)(3), csum(1)(2,3), csum(1,2)(3), or csum(1,2,3). All these methods of calling the curried function should return the same value as the original.
+ */
+
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+
+//Currying allows for partial application of functions, meaning some of the arguments can be supplied in advace, leaving th rest to be supplied later.
+
+//fn.length indicates the number of parameters expected by the function
+var curry = function(fn) {
+
+    let argsx = fn.length;
+    return function curried(...args) {
+        if (argsx <= args.length){
+            return fn(...args);
+        }
+        else{
+            return function(...argsn){
+                return curried(...args, ...argsn)
+            }
+        }
+
+    };
+};
+
+/**
+ * function sum(a, b) { return a + b; }
+ * const csum = curry(sum);
+ * csum(1)(2) // 3
+ */
