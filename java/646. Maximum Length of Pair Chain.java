33276a4f1720721f2cdf97962d65e20a45bf0986
Daniel Perilla <daniperil@hotmail.com>
2023-08-26 10:52:08 -0500

    Create 646. Maximum Length of Pair Chain.java

---
+++ java/646. Maximum Length of Pair Chain.java
@@ -0,0 +1,28 @@
+/**
+ *Problem: 646. Maximum Length of Pair Chain
+ *You are given an array of n pairs pairs where pairs[i] = [lefti, righti] and lefti < righti.
+ *A pair p2 = [c, d] follows a pair p1 = [a, b] if b < c. A chain of pairs can be formed in this fashion.
+ *Return the length longest chain which can be formed.
+ *You do not need to use up all the given intervals. You can select pairs in any order.
+ */
+
+class Solution {
+    //Greedy algorithm
+    public int findLongestChain(int[][] pairs) {
+        Arrays.sort(pairs, (a,b) -> Integer.compare(a[1],b[1]));
+
+        int cur = Integer.MIN_VALUE, ans = 0;
+
+        for(int[] pair : pairs){
+            if(cur< pair[0]){
+                cur=pair[1];
+                ans++;
+            }
+        }
+        
+        return ans;
+    }
+}
+/**
+Time Complexity: O(nlog⁡n)O(n \log n)O(nlogn)
+Space Complexity: O(1)O(1)O(1) */
