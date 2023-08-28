e2e02407a06a76f489519034674d50a9b6afacda
Daniel Perilla <daniperil@hotmail.com>
2023-08-28 08:57:19 -0500

    Create 225. Implement Stack using Queues.java

---
+++ java/225. Implement Stack using Queues.java
@@ -0,0 +1,61 @@
+/**
+ *Problem: 225. Implement Stack using Queues
+ *Implement a last-in-first-out (LIFO) stack using only two queues. The implemented stack should support all the functions of a normal stack (push, top, pop, and empty).
+ *Implement the MyStack class:
+ *void push(int x) Pushes element x to the top of the stack.
+ *int pop() Removes the element on the top of the stack and returns it.
+ *int top() Returns the element on the top of the stack.
+ *boolean empty() Returns true if the stack is empty, false otherwise.
+ *Notes:
+ *You must use only standard operations of a queue, which means that only push to back, peek/pop from front, size and is empty operations are valid.
+ *Depending on your language, the queue may not be supported natively. You may simulate a queue using a list or deque (double-ended queue) as long as you use only a queue's standard operations.
+ */
+
+class MyStack {
+    //One-Queue Approach
+
+    private Queue<Integer> q;
+
+    public MyStack() {
+        q = new LinkedList<>();
+    }
+    
+    public void push(int x) {
+        q.add(x);
+        for(int i = 1; i<q.size(); i++){
+            q.add(q.remove());
+        }
+        
+    }
+    
+    public int pop() {
+        return q.remove();
+        
+    }
+    
+    public int top() {
+        return q.peek();
+        
+    }
+    
+    public boolean empty() {
+        return q.isEmpty();     
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
+
+ /**
+ Complexity
+Time Complexity:
+O(n)O(n)O(n) for the push operation due to the rotation required to bring the last element to the front.
+O(1)O(1)O(1) for the pop, top, and empty operations.
+Space Complexity:
+O(n)O(n)O(n) for storing the elements in the queue. */
