0dda4ec59ed9d31796ea8c1e05835a4061af78bc
Daniel Perilla <daniperil@hotmail.com>
2023-12-03 15:31:51 -0500

    Create 1266. Minimum Time Visiting All Points.py

---
+++ python3/1266. Minimum Time Visiting All Points.py
@@ -0,0 +1,24 @@
+'''
+Problem: 1266. Minimum Time Visiting All Points
+On a 2D plane, there are n points with integer coordinates points[i] = [xi, yi]. Return the minimum time in seconds to visit all the points in the order given by points.
+
+You can move according to these rules:
+
+In 1 second, you can either:
+move vertically by one unit,
+move horizontally by one unit, or
+move diagonally sqrt(2) units (in other words, move one unit vertically then one unit horizontally in 1 second).
+You have to visit the points in the same order as they appear in the array.
+You are allowed to pass through points that appear later in the order, but these do not count as visits.
+'''
+
+class Solution:
+    def minTimeToVisitAllPoints(self, points: List[List[int]]) -> int:
+        #Approach: Move Diagonally as Much as Possible
+        ans = 0
+        for i in range(len(points) - 1):
+            curr_x, curr_y = points[i]
+            target_x, target_y = points[i + 1]
+            ans += max(abs(target_x - curr_x), abs(target_y - curr_y))
+        
+        return ans
